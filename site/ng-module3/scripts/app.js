f23a059cdac86632b06b2ce41d12eb6601064b63
AJ Kotzen <akotzen1@jhu.edu>
2016-10-21 09:38:05 -0400

    still setting up assignment pieces

---
+++ site/ng-module3/scripts/app.js
@@ -0,0 +1,82 @@
+(function() {
+'use strict';
+
+angular.module('NarrowItDownApp', [])
+	.controller('NarrowItDownController', NarrowItDownController)
+	.service('MenuSearchService', MenuSearchService)
+	.directive('foundItems', FoundItemsDirective);
+
+NarrowItDownController.$inject = ['MenuSearchService'];
+function ToBuyController(MenuSearchService) {
+	var searchCtrl = this;
+	
+	searchCtrl.found = [];
+
+	var promise = MenuSearchService.getMatchedMenuItems();
+	promise.then(function(data)) {
+		searchCtrl.found = data;
+	})
+
+	searchCtrl.removeItem = function (itemIndex) {
+		console.log('itemIndex', itemIndex);
+		var itemToRemove = searchCtrl.found[itemIndex];
+		console.log(itemToRemove);
+		searchCtrl.found.splice(itemIndex, 1);
+	};
+}
+
+function FoundItemsDirective() {
+  var ddo = {
+    templateUrl: 'foundItems.html',
+    scope: {
+      foundItems: '<',
+      onRemove: '&'
+    },
+    controller: ShoppingListDirectiveController,
+    controllerAs: 'list',
+    bindToController: true
+  };
+
+  return ddo;
+}
+
+MenuSearchService.$inject = ['$http'];
+function MenuSearchService($http) {
+	var service = this;
+	var endpoint = 'https://davids-restaurant.herokuapp.com/menu_items.json';
+
+	service.getMatchedMenuItems = function(searchTerm) {
+
+		var promise = $http({
+	      method: "GET",
+	      url: endpoint
+	    });
+
+	    return promise.then(function (response) {
+
+	    	var data = response.data;
+
+	    	if (!response || !response.data) {
+	    		console.log('No response found');
+	    		return [];
+	    	}
+
+	    	// process result and only keep items that match
+	    	var foundItems = [];
+	    	for (var i = 0; i < data.length; i++) {
+	    		if (data[i].indexOf(searchTerm) !== -1) {
+	    			foundItems.push(data[i]);
+	    		}
+	    	}
+
+      		// return processed items
+      		return foundItems;
+  		}
+  		.catch(function (errorResponse) {
+			console.log('An error occurred', errorResponse.message);
+		});
+	};
+
+}
+
+})();
