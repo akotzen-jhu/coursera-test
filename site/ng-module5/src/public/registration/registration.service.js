60e4c13f4f807e2b68ab0e05bea8da6f18812aec
AJ Kotzen <akotzen1@jhu.edu>
2016-10-24 16:15:30 -0400

    fix service

--- site/ng-module5/src/public/registration/registration.service.js
+++ site/ng-module5/src/public/registration/registration.service.js
@@ -9,26 +9,31 @@ RegistrationService.$inject = ['$http'];
 function MenuService($http) {
   var service = this;
 
-  service.getCategories = function () {
-    return $http.get(ApiPath + '/categories.json').then(function (response) {
-      return response.data;
+  //var endpoint = 'https://YOUR-CHOSEN-SUBDOMAIN.herokuapp.com/menu_items/SHORT-NAME.json';
+  var endpoint = 'https://davids-restaurant.herokuapp.com/menu_items.json?category=';
+  //var endpoint = 'https://davids-restaurant.herokuapp.com/menu_items/A1.json';
+
+  service.getMenuItem = function(shortName) {
+
+    var promise = $http({ method: "GET", url: endpoint + shortName }).
+    then(function(response) {
+      if (!response || !response.data || !response.data.menu_items) {
+        console.log('No category menu items found');
+        return [];
+      }
+
+      var menuItems = response.data.menu_items;
+
+      for (var i = 0; i < menuItems.length; i++) {
+        if (menuItems[i].short_name == shortName) {
+          return menuItems[i];
+        }
+      }
+      return [];
     });
-  };
-
-
-  service.getMenuItems = function (category) {
-    var config = {};
-    if (category) {
-      config.params = {'category': category};
-    }
-
-    return $http.get(ApiPath + '/menu_items.json', config).then(function (response) {
-      return response.data;
-    });
+    return promise;
   };
 
 }
-
-
 
 })();
