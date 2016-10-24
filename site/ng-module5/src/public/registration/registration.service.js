bbcca47f5b5e40622cdc13eb771ce0ef2bd842b7
AJ Kotzen <akotzen1@jhu.edu>
2016-10-24 16:46:17 -0400

    validation, service and controller signup

--- site/ng-module5/src/public/registration/registration.service.js
+++ site/ng-module5/src/public/registration/registration.service.js
@@ -8,12 +8,24 @@ angular.module('public')
 RegistrationService.$inject = ['$http'];
 function MenuService($http) {
   var service = this;
+  var hasValidMenuItem = false;
+  var menuItem = {};
 
   //var endpoint = 'https://YOUR-CHOSEN-SUBDOMAIN.herokuapp.com/menu_items/SHORT-NAME.json';
   var endpoint = 'https://davids-restaurant.herokuapp.com/menu_items.json?category=';
-  //var endpoint = 'https://davids-restaurant.herokuapp.com/menu_items/A1.json';
 
-  service.getMenuItem = function(shortName) {
+  service.getMenuItem = function() {
+    return service.menuItem;
+  };
+
+  service.hasValidMenuItem = function() {
+    return service.hasValidMenuItem === true;
+  };
+
+  service.lookupMenuItem = function(shortName) {
+
+    service.hasValidMenuItem = false;
+    service.menuItem = {};
 
     var promise = $http({ method: "GET", url: endpoint + shortName }).
     then(function(response) {
@@ -26,6 +38,8 @@ function MenuService($http) {
 
       for (var i = 0; i < menuItems.length; i++) {
         if (menuItems[i].short_name == shortName) {
+          service.hasValidMenuItem = true;
+          service.menuItem = menuItems[i];
           return menuItems[i];
         }
       }
--- site/ng-module5/src/public/signup/signup.controller.js
+++ site/ng-module5/src/public/signup/signup.controller.js
@@ -8,6 +8,8 @@ SignupController.$inject = ['RegistrationService'];
 function SignupController(RegistrationService) {
 
 	var signupCtrl = this;
+	var hasValidMenuItem = false;
+	var foundMenuItem = {};
 
 	signupCtrl.registration = {
 		account: {
@@ -21,7 +23,33 @@ function SignupController(RegistrationService) {
 		}
 	};
 
+	signupCtrl.hasValidMenuItem = function() {
+
+		var shortName = signupCtrl.registration.menuItem.shortName || '';
+		if (shortName.length === 0) {
+			hasValidMenuItem = false;
+			foundMenuItem = {};
+			return false;
+		}
+
+		if (signupCtrl.hasValidMenuItem) return true;
+
+		var menuItem = RegistrationService.lookupMenuItem(signupCtrl.registration.menuItem.shortName);
+		if (RegistrationService.hasValidMenuItem()) {
+			foundMenuItem = menuItem;
+			hasValidMenuItem = true;
+			return true;
+		}
+
+		return false;
+	}
+
 	signupCtrl.saveRegistration = function() {
+
+		if ()
+
+
+
 		RegistrationService.saveRegistration(signupCtrl.registration);
 	};
 }
