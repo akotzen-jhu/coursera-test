e6740d205623baa91a6d3da3e093dfbe4563efcf
AJ Kotzen <akotzen1@jhu.edu>
2016-10-25 13:17:45 -0400

    finished tests

--- site/ng-module5/spec/registration.service.spec.js
+++ site/ng-module5/spec/registration.service.spec.js
@@ -1,48 +1,50 @@
-describe('menuItemTests', function () {
+describe('registrationService', function () {
 
   var registrationService;
   var $httpBackend;
 
   beforeEach(function () {
     module('public');
-    // module('public', ['ui.router', 'common']);
 
     inject(function ($injector) {
       registrationService = $injector.get('RegistrationService');
       $httpBackend = $injector.get('$httpBackend');
-      //ApiBasePath = $injector.get('ApiBasePath');
     });
   });
 
-  it('should equal', function() {
+  it('should ensure jasmine is up and running', function() {
     expect(1).toEqual(1);
   });
 
-  it('should find the menu item (A1) in the result set', function() {
+  it('should find menu item (A1) in the response', function() {
 
     var endpoint = 'http://akotzen1-menu.herokuapp.com/menu_items/A1.json';
-    var response = {
-      data: { short_name: "A1" }
+    var mockResponse = { 
+      id: 1, 
+      short_name: "A1", 
+      name: "Won Ton Soup with Chicken" 
     };
 
-    $httpBackend.whenGET(endpoint).respond(response);
+    $httpBackend.whenGET(endpoint).respond(mockResponse);
+
     registrationService.lookupMenuItem('A1').then(function(response) {
-      expect(response.data).toContain(jasmine.objectContaining({short_name: "A1"}))
+      expect(response.data.short_name).toEqual('A1')
     });
+
     $httpBackend.flush();
   });
 
-  it('should not find a menu item in the result set', function() {
+  it('should generate internal server error for invalid menu item (A99)', function() {
 
     var endpoint = 'http://akotzen1-menu.herokuapp.com/menu_items/A99.json';
-    var response = {
-      data: { status: "500", error: "Internal Server Error"}
-    };
+    var mockResponse = { status: "500", error: "Internal Server Error" };
 
-    $httpBackend.whenGET(endpoint).respond(response);
+    $httpBackend.whenGET(endpoint).respond(mockResponse);
+
     registrationService.lookupMenuItem('A99').then(function(response) {
-      expect(response.data).not.toContain(jasmine.objectContaining({short_name: "A99"}))
+      expect(response.data.status).toEqual('500');
     });
+
     $httpBackend.flush();
   });
 
